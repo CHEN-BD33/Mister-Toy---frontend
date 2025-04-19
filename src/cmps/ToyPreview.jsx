3f59a357ec130215d4427dc277eaf7b588e621d2
CHEN-BD33 <chenben10.cbd@gmail.com>
2025-04-19 13:09:08 +0300

    Add img to toys

--- src/cmps/ToyPreview.jsx
+++ src/cmps/ToyPreview.jsx
@@ -1,13 +1,27 @@
 import { Link } from 'react-router-dom'
+import { useState } from 'react'
 
 export function ToyPreview({ toy }) {
+    const [isImgLoading, setImgLoading] = useState(true)
+   
+    function handleImageLoad() {
+        setImgLoading(false)
+    }
 
     return (
         <article>
             <h4>{toy.name}</h4>
+            {isImgLoading && <div className="skeleton-loader"></div>}
+                <div className="img-container">
+                    <img
+                        src={`https://robohash.org/${toy.name}?set=set4`}
+                        alt={toy.name}
+                        onLoad={handleImageLoad}
+                        style={{ display: isImgLoading ? 'none' : 'block' }}
+                    />
+                </div>
             <p>Price: <span>${toy.price.toLocaleString()}</span></p>
             <p>inStock: {toy.inStock ? 'Yes' : 'No'} </p>
-            {/* <img src={toy.imgUrl} alt={toy.name} /> */}
             {/* {toy.owner && <p>Owner: <Link to={`/user/${toy.owner._id}`}>{toy.owner.fullname}</Link></p>} */}
             <hr />
             <Link to={`/toy/edit/${toy._id}`}>Edit</Link> &nbsp; | &nbsp;
