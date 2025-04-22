207c94acab092dd9692cc8c786b0f3ef50e525c9
CHEN-BD33 <chenben10.cbd@gmail.com>
2025-04-22 08:28:59 +0300

    Add and connect backend

--- src/services/toy.service.js
+++ src/services/toy.service.js
@@ -1,10 +1,8 @@
-import { storageService } from './async-storage.service.js'
 import { utilService } from './util.service.js'
-import { userService} from './user.service.js'
+import { httpService } from './http.service.js'
 
-const STORAGE_KEY = 'toyDB'
+const BASE_URL = 'toy/'
 const labels = ['On wheels', 'Box game', 'Art', 'Baby', 'Doll', 'Puzzle', 'Outdoor', 'Battery Powered']
-_createToys()
 
 export const toyService = {
     query,
@@ -12,71 +10,28 @@ export const toyService = {
     save,
     remove,
     getEmptyToy,
-    getRandomToy,
     getDefaultFilter,
     getLabels
 }
 
 function query(filterBy = {}) {
-    return storageService.query(STORAGE_KEY)
-        .then(toys => {
-            if (!filterBy.txt) filterBy.txt = ''
-            if (!filterBy.maxPrice) filterBy.maxPrice = Infinity
-
-            let filteredToys = toys
-
-            if (filterBy.txt) {
-                const regExp = new RegExp(filterBy.txt, 'i')
-                filteredToys = filteredToys.filter(toy => regExp.test(toy.name))
-            }
-
-            if (filterBy.maxPrice !== Infinity) {
-                filteredToys = filteredToys.filter(toy => toy.price <= filterBy.maxPrice)
-            }
-
-            if (filterBy.inStock !== undefined) {
-                filteredToys = filteredToys.filter(toy => toy.inStock === filterBy.inStock)
-            }
-
-            if (filterBy.labels && filterBy.labels.length > 0) {
-                filteredToys = filteredToys.filter(toy => {
-                    return filterBy.labels.every(label => toy.labels.includes(label))
-                })
-            }
-
-            if (filterBy.sortBy) {
-                switch (filterBy.sortBy) {
-                    case 'name':
-                        filteredToys.sort((a, b) => a.name.localeCompare(b.name))
-                        break
-                    case 'price':
-                        filteredToys.sort((a, b) => a.price - b.price)
-                        break
-                    case 'created':
-                        filteredToys.sort((a, b) => a.createdAt - b.createdAt)
-                        break
-                }
-            }
-            return filteredToys
-        })
+    return httpService.get(BASE_URL, filterBy)
 }
 
 function getById(toyId) {
-    return storageService.get(STORAGE_KEY, toyId)
+    return httpService.get(BASE_URL + toyId)
+
 }
 
 function remove(toyId) {
-    return storageService.remove(STORAGE_KEY, toyId)
+    return httpService.delete(BASE_URL + toyId)
 }
 
 function save(toy) {
     if (toy._id) {
-        return storageService.put(STORAGE_KEY, toy)
+        return httpService.put(BASE_URL + toy._id, toy)
     } else {
-        // when switching to backend - remove the next line
-        toy.owner = userService.getLoggedinUser()
-        toy.createdAt = Date.now()
-        return storageService.post(STORAGE_KEY, toy)
+        return httpService.post(BASE_URL, toy)
     }
 }
 
@@ -84,46 +39,27 @@ function getEmptyToy() {
     return {
         name: '',
         price: '',
-        labels: [],
+        labels: _getRandomLabels(),
         inStock: true,
-        imgUrl: ''
     }
 }
 
-function getRandomToy() {
-    const randomLabels = []
-    for (let i = 0; i < utilService.getRandomIntInclusive(1, 3); i++) {
-        const randomLabel = labels[utilService.getRandomIntInclusive(0, labels.length - 1)]
-        if (!randomLabels.includes(randomLabel)) randomLabels.push(randomLabel)
-    }
 
-    return {
-        name: ['Fun Toy', 'Super Puzzle', 'Action Figure', 'Building Blocks', 'Remote toy', 'Talking Doll'][utilService.getRandomIntInclusive(0, 5)],
-        price: utilService.getRandomIntInclusive(10, 200),
-        labels: randomLabels,
-        inStock: Math.random() > 0.3,
-        imgUrl: '',
-        createdAt: Date.now() - utilService.getRandomIntInclusive(0, 1000 * 60 * 60 * 24 * 30)
+function _getRandomLabels() {
+    const labelsCopy = [...labels]
+    const randomLabels = []
+    for (let i = 0; i < 2; i++) {
+        const randomIdx = Math.floor(Math.random() * labelsCopy.length)
+        randomLabels.push(labelsCopy.splice(randomIdx, 1)[0])
     }
+    return randomLabels
 }
 
 function getDefaultFilter() {
-    return { txt: '', maxPrice: '', inStock: undefined, labels: [], sortBy: 'name' }
+    return { txt: '', maxPrice: '', inStock: undefined, labels: [], sortBy: 'name',sortDir: 1 }
 }
+
 
 function getLabels() {
     return [...labels]
-}
-
-function _createToys() {
-    var toys = utilService.loadFromStorage(STORAGE_KEY)
-    if (toys && toys.length > 0) return
-
-    toys = []
-    for (var i = 0; i < 12; i++) {
-        const toy = getRandomToy()
-        toy._id = utilService.makeId()
-        toys.push(toy)
-    }
-    utilService.saveToStorage(STORAGE_KEY, toys)
 }
